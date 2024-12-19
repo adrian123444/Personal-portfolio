4db76d7783be61a0427296347a1c0c39b02551c9
adrianremarkable <adrianremarkable@gmail.com>
2024-12-19 17:36:19 +0000

    readme, and storybook app

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+
+  addons: [
+    "@storybook/addon-webpack5-compiler-swc",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-mdx-gfm"
+  ],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
+};
+export default config;
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Personal-Portfolio
-adrian-savin
+# Personal Portfolio
+Developer Name: Adrian Iamandi
+From: UK
+Level: 33+
+
+Hobbies: Gaming, Coding, Sports
+
+About:
+What: I am a Front-End Web Developer.
+
+I created this app to serve as an online portfolio showcasing my past, present, and future projects.
+
+The technologies and dependencies used in this app are listed below:
+
+React
+Storybook
+Webpack 5
+TailwindCSS
+JavaScript
+DevDependencies:
+
+@chromatic-com/storybook
+@storybook/addon-essentials
+@storybook/addon-interactions
+@storybook/react
+webpack
